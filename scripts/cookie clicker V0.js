e0387a2d113bc85571dd1bc61638a96a1a8c62de
Spacetrale <spacetrale@gmail.com>
2026-04-16 22:52:22 +0200

    Update cookie clicker V0.js

--- scripts/cookie clicker V0.js
+++ scripts/cookie clicker V0.js
@@ -4,7 +4,7 @@
 
     const menu = document.createElement("div")
     menu.id = "main"
-    menu.style = "top: 50%; left: 50%;"
+    menu.style = "top: 20%; left: 20%;"
     menu.innerHTML = `
             <nav>
                 <ul id="navbar">
@@ -20,7 +20,10 @@
             </nav>
             <div id="menu-container">
                 <div class="menu active">
-                    <h2>Not ready</h2>
+                    <input id="cookies-entry" type="number" class="entry" placeholder="Cookies...">
+                    <button id="cookies-btn" class="btn">Give cookies</button>
+                    <input id="cps-entry" type="number" class="entry" placeholder="Cps...">
+                    <button id="cps-btn" class="btn">Set cps</button>
                 </div>
                 <div class="menu">
                     <h2>Not ready</h2>
@@ -42,7 +45,19 @@
     notifications.id = "notifications"
     document.body.appendChild(notifications)
 
-    // Base Function
+    // Script Functions
+
+    const cookiesEntry = menu.querySelector("#cookies-entry")
+    const cpsEntry = menu.querySelector("#cps-entry")
+
+    menu.querySelector("#cookies-btn").addEventListener("click", () => {
+        const result = parseInt(cookiesEntry.textContent)
+        if (isNaN(result)) {
+            createNotification("?", "Problem", "You have put a string or none value.")
+        }
+    })
+
+    // Base Functions
     const moveBtn = menu.querySelector("#move")
     let moveState = false
 
@@ -51,8 +66,8 @@
     })
     document.addEventListener("mousemove", (e) => {
         if (moveState == true) {
-            menu.style.top = `${e.clientY-25}px`
-            menu.style.left = `${e.clientX-menu.clientWidth+25}px`
+            menu.style.top = `${e.clientY - 25}px`
+            menu.style.left = `${e.clientX - menu.clientWidth + 25}px`
         }
     })
     moveBtn.addEventListener("mouseup", () => {
@@ -70,7 +85,33 @@
         allButtons[n].classList.add("active")
         allMenus[n].classList.add("active")
     }
-    
+
+    function createNotification(icon, type, description) {
+        const notification = document.createElement("div")
+        notification.classList.add("notification")
+        notification.innerHTML = `
+            <div class="icon ${type.toLowerCase()}">
+                <h2>${icon}</h2>
+            </div>
+            <div id="info">
+                <span>${type}:</span>
+                <p>${description}</p>
+            </div>`
+        notification.style.animation = "newNotification 2s fowards"
+        notifications.appendChild(notification)
+
+
+        setTimeout(() => {
+            notification.style.opacity = "0"
+
+            setTimeout(() => {
+                notification.remove()
+            }, 500)
+        }, 7000)
+    }
+
+    // INIT
+
     let allButtons = menu.querySelectorAll("li")
     for (let i = 0; i < allButtons.length; i++) {
         allButtons[i].addEventListener("click", () => {
